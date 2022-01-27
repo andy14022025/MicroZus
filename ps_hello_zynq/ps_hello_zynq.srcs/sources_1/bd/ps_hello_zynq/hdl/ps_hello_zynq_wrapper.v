f5e52b5d83dcf6e8069d6d6b17f2de05a992bd00
Andy <andy14022025@msn.com>
2022-01-28 00:56:16 +0800

    6.ps_helloworld

---
+++ ps_hello_zynq/ps_hello_zynq.srcs/sources_1/bd/ps_hello_zynq/hdl/ps_hello_zynq_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+//Date        : Fri Jan 28 00:30:06 2022
+//Host        : DESKTOP-TM97VDU running 64-bit major release  (build 9200)
+//Command     : generate_target ps_hello_zynq_wrapper.bd
+//Design      : ps_hello_zynq_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module ps_hello_zynq_wrapper
+   ();
+
+
+  ps_hello_zynq ps_hello_zynq_i
+       ();
+endmodule
---
+++ ps_hello_zynq/ps_hello_zynq.srcs/sources_1/bd/ps_hello_zynq/synth/ps_hello_zynq.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+//Date        : Fri Jan 28 00:30:06 2022
+//Host        : DESKTOP-TM97VDU running 64-bit major release  (build 9200)
+//Command     : generate_target ps_hello_zynq.bd
+//Design      : ps_hello_zynq
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* CORE_GENERATION_INFO = "ps_hello_zynq,IP_Integrator,{x_ipVendor=xilinx.com,x_ipLibrary=BlockDiagram,x_ipName=ps_hello_zynq,x_ipVersion=1.00.a,x_ipLanguage=VERILOG,numBlks=1,numReposBlks=1,numNonXlnxBlks=0,numHierBlks=0,maxHierDepth=0,numSysgenBlks=0,numHlsBlks=0,numHdlrefBlks=0,numPkgbdBlks=0,bdsource=USER,synth_mode=Global}" *) (* HW_HANDOFF = "ps_hello_zynq.hwdef" *) 
+module ps_hello_zynq
+   ();
+
+
+  ps_hello_zynq_processing_system7_0_0 processing_system7_0
+       ();
+endmodule
